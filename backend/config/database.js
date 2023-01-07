33b1a62b5231cd1cb7667b43af3ad69c506c6065
Lukáš Franc <lf@antstudio.cz>
2023-01-07 16:37:30 +0100

    [Backend] basic structure, configuration, graphql plugin, cloudinary, ckeditor

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'backend'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
+      host: env('PGHOST', 'localhost'),
+      port: env.int('PGPORT', 3306),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'root'),
+      password: env('PGPASSWORD', 'root'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
